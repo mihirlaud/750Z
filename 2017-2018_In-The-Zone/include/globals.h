d75af4ee2c28fa85a8c9b661673ec0113fd55155
al-li-son <36108590+al-li-son@users.noreply.github.com>
2018-02-09 20:59:17 -0500

    Update globals.h

--- 2017-2018_In-The-Zone/include/globals.h
+++ 2017-2018_In-The-Zone/include/globals.h
@@ -1,10 +1,15 @@
 #define D_MOTOR_1 2 //Assigns drive motors to ports 2 - 5
 #define D_MOTOR_2 3
 #define D_MOTOR_3 4
-#define D_MOTOR_4 5
+#define D_MOTOR_4 1
 
 #define L_MOTOR_1 6 //Assigns lift motors to ports 6 and 7
 #define L_MOTOR_2 7
+
+#define C_MOTOR_1 8 //Assigns claw arm motors to ports
+#define C_MOTOR_2 5
+
+#define CLAW_MOTOR 10 //Assigns claw motor to
 
 #define JOYSTICK 1 //Defines other constants
 #define L_AXIS 1
@@ -13,4 +18,6 @@
 void drive(int x, int y);
 //void turn(int rotation);
 void lift(bool extend);
+void swing(bool forward);
+void claw(bool close);
 void memes();
